e18140a1bcbb7a855988bbb8e64d8c328e34cf48
ahmadabdallahh <ahmadabhossen@gmail.com>
2025-02-09 20:26:28 +0200

    `Added print statements and conditional blocks to #003/Syntax-And-Your-First-App.py`

--- #003/Syntax-And-Your-First-App.py
+++ #003/Syntax-And-Your-First-App.py
@@ -1,5 +1,18 @@
 # Syntax And Your First App
 # print() => built in function in Python to print anyThing
 
-print("Hello from python file.")
+print("Hello from python file.") 
 print("Hello world!")
+
+# only one print statement is allowed in a line
+# print("Hello world!", "Hello from python file.")  # this will throw an error
+print("one") ; print("two")
+
+if True :
+    print("Hello world")
+    print("Hello world")
+
+if True :
+    # inside the block of code
+    print("Hello world")
+    print("Hello world")
